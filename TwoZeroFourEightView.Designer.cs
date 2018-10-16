d6e9e1b50482032ebe3e8ca62b2f750fcbc052d1
NANTHASAK SAMADSINWANICH <nanthasak_sa@cmu.ac.th>
2018-10-16 14:31:12 +0700

    DONE 100%

--- TwoZeroFourEightView.Designer.cs
+++ TwoZeroFourEightView.Designer.cs
@@ -254,6 +254,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
             this.ClientSize = new System.Drawing.Size(370, 428);
             this.Controls.Add(this.scoredisplay);
             this.Controls.Add(this.Score);
